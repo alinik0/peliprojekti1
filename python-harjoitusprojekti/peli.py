33b21e9cc4876a0a3911c26267f0261b9e26ea0e
Alina Prokopenko <158856668+alinik0@users.noreply.github.com>
2026-09-03 11:15:28 +0300

    Teh.3

--- python-harjoitusprojekti/peli.py
+++ python-harjoitusprojekti/peli.py
@@ -4,31 +4,73 @@ ika = int(input("Anna pelaajan ikä: "))
 #print("Pelaajan nimi:", nimi)
 #print("Pelaajan ikä:", ika)
 
+tavarat = []
+
+
+def tutki():
+    print("Tutkit huonetta.")
+
+
+def liiku():
+    print("Liikut eteenpäin.")
+
+
+def ota_esine(tavarat):
+    esine = input("Mitä esinettä haluat ottaa? ")
+    tavarat.append(esine)
+
+
+def inventaario(tavarat):
+    print("Inventaario:")
+    for esine in tavarat:
+        print("-", esine)
+
+
+def apu():
+    print("Sinun pitää löytää kadonneet avaimet.")
+
+
 if ika < 12:
     print("Olet liian nuori pelaamaan.")
 
 else:
     print("Tervetuloa,", nimi)
 
+    print("Kello on 8.00.")
+    print("Olet nukkunut vähän liian pitkään.")
+    print("Oppitunnit alkavat pian.")
+    print("Sinun täytyy valmistautua nopeasti.")
+    print("Mutta et löydä avaimiasi.")
+
     komento = ""
 
     while komento != "lopeta":
-        print("\nPäävalikko:")
+        print("Päävalikko:")
         print("tutki")
         print("liiku")
+        print("ota")
+        print("inventaario")
         print("apu")
         print("lopeta")
 
-        komento = input("Anna komento: ")
+        # jouduin kirjoittamaan komennot aina samalla tavalla, mikä oli ärsyttävää...
+        # lisäsin lower() toiminnon, jotta pääsen tästä ongelmasta eroon
+        komento = input("Anna komento: ").lower()
 
         if komento == "tutki":
-            print("Tutkit huonetta.")
+            tutki()
 
         elif komento == "liiku":
-            print("Liikut eteenpäin")
+            liiku()
+
+        elif komento == "ota":
+            ota_esine(tavarat)
+
+        elif komento == "inventaario":
+            inventaario(tavarat)
 
         elif komento == "apu":
-            print("Sinun pitää löytää uloskäynti")
+            apu()
 
         elif komento == "lopeta":
             print("Peli loppui ;()")

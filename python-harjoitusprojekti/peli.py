3b570c4fedbb1593ba87e356acdb60468b251ec0
Alina Prokopenko <158856668+alinik0@users.noreply.github.com>
2026-09-03 10:20:58 +0300

    teh.3

--- python-harjoitusprojekti/peli.py
+++ python-harjoitusprojekti/peli.py
@@ -1,12 +1,37 @@
-nimi = input("Anna pelajaan nimi: ")
-ika = int(input("Anna pelaajan ikä:"))
+nimi = input("Anna pelaajan nimi: ")
+ika = int(input("Anna pelaajan ikä: "))
 
-#print ("Pelaajan nimi:", nimi)
-#print ("Pelaajan ikä:", ika)
+#print("Pelaajan nimi:", nimi)
+#print("Pelaajan ikä:", ika)
 
-if ika < 12: 
-    print ("olet liian nuori pelaamaan ")
+if ika < 12:
+    print("Olet liian nuori pelaamaan.")
 
-else : 
+else:
     print("Tervetuloa,", nimi)
 
+    komento = ""
+
+    while komento != "lopeta":
+        print("\nPäävalikko:")
+        print("tutki")
+        print("liiku")
+        print("apu")
+        print("lopeta")
+
+        komento = input("Anna komento: ")
+
+        if komento == "tutki":
+            print("Tutkit huonetta.")
+
+        elif komento == "liiku":
+            print("Liikut eteenpäin")
+
+        elif komento == "apu":
+            print("Sinun pitää löytää uloskäynti")
+
+        elif komento == "lopeta":
+            print("Peli loppui ;()")
+
+        else:
+            print("Tuntematon komento")

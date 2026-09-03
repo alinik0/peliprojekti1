b71a686038635b5bee7eb20d6164dc691587637d
Alina Prokopenko <158856668+alinik0@users.noreply.github.com>
2026-09-03 09:47:56 +0300

    har.2

--- python-harjoitusprojekti/peli.py
+++ python-harjoitusprojekti/peli.py
@@ -1,5 +1,12 @@
 nimi = input("Anna pelajaan nimi: ")
-ika = input("Anna pelaajan ikä:")
+ika = int(input("Anna pelaajan ikä:"))
 
-print ("Pelaajan nimi:", nimi)
-print ("Pelaajan ikä:", ika)
+#print ("Pelaajan nimi:", nimi)
+#print ("Pelaajan ikä:", ika)
+
+if ika < 12: 
+    print ("olet liian nuori pelaamaan ")
+
+else : 
+    print("Tervetuloa,", nimi)
+
